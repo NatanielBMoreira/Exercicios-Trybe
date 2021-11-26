09f4d3f64051e7a4fb6c95c94ac3083470c40a51
Nataniel <natanielbmoreira@gmail.com>
2021-11-26 20:06:49 -0300

    add read.me e script maior número.

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-4.1-javascript-primeiros-passos/Exercicios/exercicio-2/script.js
@@ -0,0 +1,12 @@
+const nunber1 = 100;
+const nunber2 = 150;
+
+
+if(nunber1 > nunber2)
+{
+    console.log('O número ' +nunber1+ ' é o maior');
+}
+else
+{
+    console.log('O número ' + nunber2 + ' é o maior');  
+}
