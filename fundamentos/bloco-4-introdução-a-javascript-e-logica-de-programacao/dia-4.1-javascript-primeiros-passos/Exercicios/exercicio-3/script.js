15449b263eb2e9d8cbaa9358f61009259afb4665
Nataniel <natanielbmoreira@gmail.com>
2021-11-26 20:11:04 -0300

    add read.me e script maior número entre três números.

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-4.1-javascript-primeiros-passos/Exercicios/exercicio-3/script.js
@@ -0,0 +1,17 @@
+const nunber1 = 100;
+const nunber2 = 150;
+const nunber3 = 120;
+
+
+if(nunber1 > nunber2 && nunber1 > nunber3) 
+{
+    console.log('O número ' +nunber1+ ' é o maior');
+}
+else if(nunber2 > nunber3)
+{
+    console.log('O número ' + nunber2 + ' é o maior');  
+}
+else
+{
+    console.log('O número ' + nunber3 + ' é o maior');
+}
