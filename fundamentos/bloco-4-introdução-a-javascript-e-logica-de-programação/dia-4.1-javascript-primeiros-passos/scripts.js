6c34d0c9db0fb5a042d0c612eb53b741ec3ae845
Nataniel <natanielbmoreira@gmail.com>
2021-11-26 15:20:57 -0300

    Primeiro Na pratica.

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programação/dia-4.1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,15 @@
+const myName = "Nataniel";
+const birthCity = "Canguçu";
+let birthYear = 1999;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Porto Alegre";     //Não podemos alterar o valor de uma const;
+
+console.log(birthCity);
