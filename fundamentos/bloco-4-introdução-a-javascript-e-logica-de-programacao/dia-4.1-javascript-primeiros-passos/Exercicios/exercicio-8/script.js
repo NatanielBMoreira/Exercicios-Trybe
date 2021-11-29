421d320b371e01556c581a18ce53309834ff94a2
Nataniel <natanielbmoreira@gmail.com>
2021-11-29 18:58:38 -0300

    Exercício 8 concluído.

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-4.1-javascript-primeiros-passos/Exercicios/exercicio-8/script.js
@@ -0,0 +1,12 @@
+const number1 = 26;
+const number2 = 27;
+const number3 = 25;
+
+if((number1 % 2) === 0 || (number2 % 2) === 0 || (number3 % 2) === 0)
+{
+    console.log('true');
+}
+else
+{
+    console.log('false');
+}
