bdc197cda25e047a766bc1910c9d4a71507e1a5a
Nataniel <natanielbmoreira@gmail.com>
2021-11-30 18:40:52 -0300

    Exercício 3 concluído.

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/exercicio-3/script.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sun = 0;
+
+for (let index = 0 ; index < numbers.length ; index +=1)
+{
+    sun = sun + numbers[index];
+}
+
+let media = sun / numbers.length;
+console.log(media);
+
+
+
+
